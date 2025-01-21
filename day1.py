f45edd7ce49866e4715bd7634802ef20d2ce67c3
pjknsiah <viserionthewraith.121@gmail.com>
2025-01-21 18:50:06 +0000

    some improvements

--- day1.py
+++ day1.py
@@ -6,8 +6,24 @@ def main():
             b = int(input("Enter a second number: "))
             break
         except ValueError:
-            print("Invalid input")
-    print(divide(a,b))
+            print("Invalid input. Enter a valid integer")
+    print("Select an operation\n1. Add\n2. Multiply\n3. Subtract\n4. Divide")
+    while True:
+        try:
+            choice = int(input("Enter your choice (1-4): "))
+            if choice in [1, 2, 3, 4]:
+                break
+        except ValueError:
+            print("Invalid input. Enter a valid integer between 1 and 4")
+    if choice == 1:
+        result = add(a, b)
+    elif choice == 2:
+        result = multiply(a, b)
+    elif choice == 3:    
+        result = subtract(a, b)
+    else:
+        result = divide(a, b)
+    print(f"The result of the operation is {result}")
 
 def add(a, b):
     return a + b
