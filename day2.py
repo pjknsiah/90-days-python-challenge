a310decb9064294aef7d1e8d5056d41f0e2cd5ae
pjknsiah <viserionthewraith.121@gmail.com>
2025-02-02 17:03:45 +0000

    final touch

--- day2.py
+++ day2.py
@@ -1,10 +1,14 @@
+import datetime
+
 def main():
     name = input("Enter your name: ")
     try:
         age = int(input("How old are you: "))
     except ValueError:
         return"Invalid age input"
-    print(f"Hello, {name}. You were born in ")
+    currentYear = datetime.date.today().year
+    birthYear = currentYear - age
+    print(f"Hello, {name}. You were born in {birthYear} ")
 
 if __name__ == "__main__":
     main()
