9b4a4bdf647b4cf5ed61a486713e5ad83e7effae
pjknsiah <viserionthewraith.121@gmail.com>
2025-02-10 22:01:40 +0000

    initial commit

--- day7.py
+++ day7.py
@@ -1,3 +1,11 @@
 def main():
-    userInfo = {}
-    name = input("Enter your name and age in the format name:age ")
+    userInfo = {'Victoria': 1234, 'John': 5678, 'Marie': 9101, 'Pierre': 1121, 'Lucius': 3141}
+    username = input("Enter your username: ")
+    if username not in userInfo:
+        print("Username not found")
+    else:
+        print(userInfo[username])
+
+if __name__ == "__main__":
+    main()
+    
