f815f7df0cca24545222eb70e2bc9f67309df72c
pjknsiah <viserionthewraith.121@gmail.com>
2025-02-10 22:03:28 +0000

    minor modifications

--- day7.py
+++ day7.py
@@ -1,5 +1,5 @@
 def main():
-    userInfo = {'Victoria': 1234, 'John': 5678, 'Marie': 9101, 'Pierre': 1121, 'Lucius': 3141}
+    userInfo = {'Victoria': 1234, 'John': 5678, 'Marie': 9101, 'Pierre': 1121, 'Lucius': 3141, 'Nero': 5161}
     username = input("Enter your username: ")
     if username not in userInfo:
         print("Username not found")
